05362b6d52875a0e200f7cdeddf2b6566fae69d4
jp <jp.ehret@laposte.net>
2020-07-05 11:41:03 +1200

    decoupage microservice

---
+++ src/main/java/com/mycompany/invoise/invoice/InvoiceApplication.java
@@ -0,0 +1,19 @@
+package com.mycompany.invoise.invoice;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceApplication.class, args);
+	}
+
+	@Bean
+	public Hibernate5Module datatypeHibernateModule() {
+		return new Hibernate5Module();
+	}
+}
