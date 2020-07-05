41f17ffd0702e3551f59f848be9d43c3d8102a8d
jp <jp.ehret@laposte.net>
2020-07-05 12:42:12 +1200

    entités et données

--- src/main/java/com/mycompany/invoise/invoice/InvoiceApplication.java
+++ src/main/java/com/mycompany/invoise/invoice/InvoiceApplication.java
@@ -3,9 +3,11 @@ package com.mycompany.invoise.invoice;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EntityScan("com.mycompany.invoise.core.entity.invoice")
 public class InvoiceApplication {
 
 	public static void main(String[] args) {
